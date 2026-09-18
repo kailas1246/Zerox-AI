1a702d154c8a23e5a760f9f64a274b7a6ccad91b
kailas1246 <kailasgrtvm@gmail.com>
2026-09-18 05:49:42 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-export const GEMINI_API_KEY = import.meta.env.VITE_GEMINI_API_KEY;
+export const GEMINI_API_KEY = import.meta.env.GEMINI_API_KEY;
 
-export const UNSPLASH_ACCESS_KEY = import.meta.env.VITE_UNSPLASH_ACCESS_KEY;
+export const UNSPLASH_ACCESS_KEY = import.meta.env.UNSPLASH_ACCESS_KEY;
